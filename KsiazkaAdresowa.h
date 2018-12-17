cd566f05fbc2bc78cb749e4b4cb7c68e8ce00465
lukaszWerdymora <lukasz.jaworski.programista@gmail.com>
2018-12-17 20:41:27 +0100

    Created base class KsiazkaAdresowa and Usytkownik

---
+++ KsiazkaAdresowa.h
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+
+#include "Uzytkownik.h"
+using namespace std;
+
+class KsiazkaAdresowa {
+    int idZalogowanegoUzytkownika;
+    int idOstatniegoAdresata;
+    int idUsunietegoAdresata;
+    vector <Uzytkownik> uzytkownicy;
+public:
+    void rejstracjaUzytkownika();
+};
---
+++ Uzytkowni.h
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+class Uytkownik {
+    int id;
+    string login, haslo;
+};
