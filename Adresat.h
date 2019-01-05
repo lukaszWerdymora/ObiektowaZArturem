faf7522766f15bb5375069329f43a35a062c71fa
lukaszWerdymora <lukasz.jaworski.programista@gmail.com>
2019-01-05 21:18:29 +0100

    add Adresat

---
+++ Adresat.h
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+class Adresat {
+    int id, idUzytkownika;
+    string imie, nazwisko, numerTelefonu, email, adres;
+public:
+    void pobierzId (int noweId);
+
+};
